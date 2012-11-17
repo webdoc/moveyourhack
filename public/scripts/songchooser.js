9316f67337dd09582ca401c8864cdddeecddfc60
Olivier Amblet <olivier@amblet.net>
2012-11-17 18:16:29 +0100

    Working search

--- public/scripts/songchooser.js
+++ public/scripts/songchooser.js
@@ -1,20 +1,48 @@
 function SongChooser(options){
 
-  function initialize(options) {
+  var handleSearchResult = function(results){
+    console.log(results) ;
+    var html = '<table class="table search-result-list">' ;
+    html += '<tr><th>Song</th><th>Artist</th><th>Album</th></tr>' ;
+    for(var i = 0; i < results.data.length; i++) {
+      var track = results.data[i] ;
+      if(track){
+        html += '<tr class="result-line">'
+        html += '<td><div class="track">' + track.title + '</div></td>' ;
+        html += '<td><div class="artist">' + track.artist.name + '</div></td>' ;
+        html += '<td><div class="album">' + track.album.title + '</div></td>' ;
+        html += '</tr>'
+      }
+    }
+    html += '</table>' ;
+    jQuery(options.searchResultNode).html(html);
+    jQuery('tr', options.searchResultNode)
+  } ;
+
+  var initialize = function(options) {
     jQuery(options.dezzerNode).html('<div id="dz-root"></div>') ;
     DZ.init({
       appId  : '108771',
       channelUrl : 'http://localhost:3000/dezzer/channel.html'
     });
-    jQuery(options.songChooserNode).html('<form class="song-chooser-search"><input type="text" placeholder="Song Title" /><input type="submit" value="Search"></form>') ;
+    jQuery(options.songChooserNode).html(
+      '<form class="song-chooser-search">'+
+      '  <div class="input-append">'+
+      '    <input type="text" placeholder="Song Title" />'+
+      '    <button class="btn" type="button">Go!</button>'+
+      '  </div>'+
+      '</form>') ;
 
     var form = jQuery('.song-chooser-search') ;
-    form.submit(function(e) {
+    var handleSearch = function(e) {
       e.preventDefault();
       var query = jQuery('input', form).attr('value');
       if(!query) { return }
-      DZ.api('search?q=' + query, function(res){ console.log(res) }) ;
-    }) ;
+      DZ.api('search?q=' + query, handleSearchResult) ;
+    } ;
+
+    form.on('submit', handleSearch);
+    jQuery('button', form).on('click', handleSearch)
   }
 
   initialize.apply(this, arguments);
