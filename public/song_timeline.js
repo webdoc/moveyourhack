be9ef2f7d201aaf39b2c965d7e03736431c2037e
Julien Bachmann <julien.bachmann@webdoc.com>
2012-11-17 22:20:47 +0100

    fix timeline when sample interval is bigger

--- public/song_timeline.js
+++ public/song_timeline.js
@@ -8,65 +8,44 @@ function SongTimeline(song, interval) {
 
 SongTimeline.prototype.init = function() {
 	var result = jQuery.Deferred();
-	var currentSample = 1;
 	var result = jQuery.Deferred();
-	this.song.getProfile().done(function(analyse){
-		// BEATS
-		var count = analyse.beats.length;
+	var sampleValue;
+
+	var filltimeline = function(timeline, data, fullLength) {
+		var count = data.length;
+		var currentSample = 1;
 		var i;
 		for (i = 0; i < count; i++) {
+			if (currentSample * this.interval * 0.001 > data[i].start) {
+				continue;
+			}
 			//create 0 sample before this point
-			while (currentSample * this.interval * 0.001 < analyse.beats[i].start) {
-				this.beatTimeline[currentSample - 1] = 0;
+			while (currentSample * this.interval * 0.001 < data[i].start) {				
+				timeline[currentSample - 1] = 0;
 				currentSample++;
 			}
-			this.beatTimeline[currentSample - 1] = 1;
+			sampleValue = (data[i].confidence > 0.5) ? 1 : 0;
+			timeline[currentSample - 1] = sampleValue;
 			currentSample++;
 		}
 		// fill with empty until the end
-		while(currentSample * this.interval * 0.001 < analyse.meta.seconds) {
-			this.beatTimeline[currentSample - 1] = 1;
+		while(currentSample * this.interval * 0.001 < fullLength) {
+			timeline[currentSample - 1] = 1;
 			currentSample++;			
 		}
-		console.log(this.beatTimeline, this.beatTimeline.length);
+		console.log(timeline, timeline.length);
+	}.bind(this);
 
+	this.song.getProfile().done(function(analyse){
+		// BEATS
+		filltimeline(this.beatTimeline, analyse.beats, analyse.meta.seconds);
+		
 		// TATUMS
-		count = analyse.tatums.length;
-		currentSample = 1;
-		for (i = 0; i < count; i++) {
-			//create 0 sample before this point
-			while (currentSample * this.interval * 0.001 < analyse.tatums[i].start) {
-				this.tatumsTimeline[currentSample - 1] = 0;
-				currentSample++;
-			}
-			this.tatumsTimeline[currentSample - 1] = 1;
-			currentSample++;
-		}
-		// fill with empty until the end
-		while(currentSample * this.interval * 0.001 < analyse.meta.seconds) {
-			this.tatumsTimeline[currentSample - 1] = 1;
-			currentSample++;			
-		}		
-		console.log(this.tatumsTimeline, this.tatumsTimeline.length);
+		filltimeline(this.tatumsTimeline, analyse.tatums, analyse.meta.seconds);
 
 		// SECTIONS
-		count = analyse.sections.length;
-		currentSample = 1;
-		for (i = 0; i < count; i++) {
-			//create 0 sample before this point
-			while (currentSample * this.interval * 0.001 < analyse.sections[i].start) {
-				this.sectionsTimeline[currentSample - 1] = 0;
-				currentSample++;
-			}
-			this.sectionsTimeline[currentSample - 1] = 1;
-			currentSample++;
-		}		
-		// fill with empty until the end
-		while(currentSample * this.interval * 0.001 < analyse.meta.seconds) {
-			this.sectionsTimeline[currentSample - 1] = 1;
-			currentSample++;			
-		}
-		console.log(this.sectionsTimeline, this.sectionsTimeline.length);	
+		filltimeline(this.sectionsTimeline, analyse.sections, analyse.meta.seconds);
+	
 		result.resolve();	
 		
 	}.bind(this));
