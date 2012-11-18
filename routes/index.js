05f83c19ec9fdf74a5b353008e1b429758b77309
Noé Froidevaux <noe.froidevaux@webdoc.com>
2012-11-18 10:44:24 +0000

    Pass all config in view...

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,19 @@ var config = require('../config');
 
 exports.index = function(req, res){
   console.log(req.session);
-  res.render('index', { title: 'Express', auth: req.session.passport, host: config.web.host});
+  res.render('index', { title: 'Express', auth: req.session.passport, config: config });
+};
+
+exports.logout = function(req, res){
+  req.logout();
+  res.redirect('/');
+};
+
+var config = require('../config');
+
+exports.index = function(req, res){
+  console.log(req.session);
+  res.render('index', { title: 'Express', auth: req.session.passport, config: config });
 };
 
 exports.logout = function(req, res){
