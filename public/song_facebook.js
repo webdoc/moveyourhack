dbba1270c8d229e5a7f2477e752272e32d728d8c
Noé Froidevaux <noe.froidevaux@webdoc.com>
2012-11-18 11:01:18 +0000

    Add song facebook

--- public/song_facebook.js
+++ public/song_facebook.js
@@ -1,4 +1,23 @@
 window.songFacebook = {
   ditInit: function(){
+    this.init = true;
+  },
+
+  publishInFeed: function(score) {
+    var obj = {
+      method: 'feed',
+      redirect_uri: 'http://www.danceparty.me',
+      link: 'http://www.danceparty.me',
+      //picture: 'http://fbrell.com/f8.jpg',
+      name: 'Dance Party!',
+      caption: 'My score: '+score,
+      description: 'Dance on your sounds.'
+    };
+
+    function callback(response) {
+      console.log('FB publish ok');
+    }
+
+    FB.ui(obj, callback);
   }
 };
